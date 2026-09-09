c4ee192269597790ee2ccb61c743aef07f0bdc2f
长安 <1420970597@qq.com>
2026-09-09 10:00:28 +0800

    chore: remove MVP implementation, keep documentation deliverables

--- controlled_data_mvp/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Authorized, non-sensitive training-data preparation MVP."""
-
-from .service import ControlledDataService, PolicyBlockedError, ValidationError
-
-__all__ = ["ControlledDataService", "PolicyBlockedError", "ValidationError"]
